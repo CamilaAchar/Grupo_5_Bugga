8b3f8153e2ee4234a1caf8e355efa147cd6144aa
lucas <lucas.galletti@gmail.com>
2021-10-09 19:34:22 -0300

    login registro userProfile

--- app.js
+++ app.js
@@ -8,4 +8,7 @@ app.use(express.static(publicPath));
 app.listen(3080, ()=> console.log('Servidor OK - Bugga - 3080'));
 
 app.get('/', (req,res)=>res.sendFile(path.join(__dirname, './views/login.html')));
-app.get('/home', (req,res)=>res.sendFile(path.join(__dirname, './views/registro.html')));
+app.get('/home', (req,res)=>res.sendFile(path.join(__dirname, './views/login.html')));
+app.get('/login', (req,res)=>res.sendFile(path.join(__dirname, './views/login.html')));
+app.get('/register', (req,res)=>res.sendFile(path.join(__dirname, './views/registro.html')));
+app.get('/userProfile', (req,res)=>res.sendFile(path.join(__dirname, './views/userProfile.html')));
