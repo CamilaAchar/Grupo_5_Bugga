579ab710ac055721b8808a786ba46a627bb99022
CamilaAchar <achartrombacamila@gmail.com>
2021-10-13 21:04:58 -0300

    agregando Home + Detalle

--- app.js
+++ app.js
@@ -8,4 +8,7 @@ app.use(express.static(publicPath));
 app.listen(3080, ()=> console.log('Servidor OK - Bugga - 3080'));
 
 app.get('/', (req,res)=>res.sendFile(path.join(__dirname, './views/login.html')));
-app.get('/home', (req,res)=>res.sendFile(path.join(__dirname, './views/registro.html')));
+app.get('/login', (req,res)=>res.sendFile(path.join(__dirname, './views/registro.html')));
+app.get('/detalleDelProducto', (req,res)=>res.sendFile(path.join(__dirname, './views/productDetail.html')));   
+app.get('/home', (req,res)=>res.sendFile(path.join(__dirname, './views/home.html'))); 
+app.get('/productos', (req,res)=>res.sendFile(path.join(__dirname, './views/productos.html'))); 
